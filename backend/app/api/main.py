f754138b85eeecc7b36f719c3d2dbf3a658bf585
Jacob Valdez <jacobfv123@gmail.com>
2024-05-12 10:40:05 -0500

    save

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -22,31 +22,13 @@ def health() -> str:
 
 api_router.get("/status", tags=["meta"])
 def get_status() -> dict:
-    return {
-        "status": state.status,
-        "uptime": (datetime.utcnow() - state.backend_start).total_seconds(),
-    }
+    return state.status
 
-api_router.get("/info", tags=["meta"])
-def get_info() -> dict:
-    return {
-        "service": "Web2 App Backend",
-        "version": "0.0.1",
-        "status": state.status,
-        "uptime": (datetime.utcnow() - state.backend_start).total_seconds(),
-    }
+api_router.get("/uptime", tags=["meta"])
+def get_uptime() -> str:
+    return (datetime.utcnow() - state.backend_start).total_seconds()
 
 @api_router.get("/time")
 def get_current_time() -> dict:
     """Endpoint to get the current server time."""
     return {"current_time": datetime.utcnow().isoformat()}
-
-@api_router.get("/config")
-def get_config() -> dict:
-    """Endpoint to get the current configuration settings."""
-    return {
-        "email_enabled": settings.emails_enabled,
-        "smtp_host": settings.SMTP_HOST,
-        "smtp_port": settings.SMTP_PORT
-    }
-
